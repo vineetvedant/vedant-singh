d010d755eafea78eb86ba7ad52ae752d3794bdc9
vineetvedant <56888751+vineetvedant@users.noreply.github.com>
2022-07-03 12:14:22 +0530

    Create google_meet_bot.py
    it will help you to join the meeting . by just setting time and mouse location over the screen . i hope you will like my project

---
+++ google_meet_bot.py
@@ -0,0 +1,23 @@
+from time import sleep
+import pyautogui as auto
+import schedule
+import webbrowser
+
+link =  input("please enter your class link :- ")
+
+time = input("plesee enter your time ") 
+
+
+def join():
+    webbrowser.open_new_tab('https://' + link)
+    sleep(15)
+    
+    auto.hotkey('ctrl','d')
+    auto.hotkey('ctrl','e')
+    auto.click(1008,445)
+
+schedule.every().day.at(time).do(join)
+
+while True:
+    schedule.run_pending()
+    sleep(1)
